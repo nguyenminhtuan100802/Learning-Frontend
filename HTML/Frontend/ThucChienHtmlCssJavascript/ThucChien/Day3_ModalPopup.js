5707ad6eaeca1e2d3aefb44f989c1a012ac283cb
nguyenminhtuan100802 <139602897+nguyenminhtuan100802@users.noreply.github.com>
2024-07-27 21:34:33 +0700

    27/7/24 - Save html lesson and 1.5 projects

---
+++ HTML/Frontend/ThucChienHtmlCssJavascript/ThucChien/Day3_ModalPopup.js
@@ -0,0 +1,10 @@
+function visiblePopUp() {
+  document.getElementById("box_pop_up").style.opacity = "1";
+  document.getElementById("box_pop_up").style.transform = "translateY(50px)";
+
+}
+
+function hiddenPopUp() {
+  document.getElementById("box_pop_up").style.opacity = "0";
+  document.getElementById("box_pop_up").style.transform = "translateY(-50px)";
+}
